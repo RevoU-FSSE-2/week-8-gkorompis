9d4a1f3342ed768b0ec18322cc4027c6e495ec0e
gkorompis <gracia.korompis@alumni.i3l.ac.id>
2023-08-07 11:45:51 +0700

    S1 add page 1 of 7: Transactions
    S1 add component: service-navbar and derrivaties, service-board and derrivatives
    
    S2 add ts logic: dom manipulation form modal, add new transaction, process form payload

---
+++ README.md
@@ -0,0 +1,67 @@
+# [Wisespent](https://64cd3acbdad64a3cc32bab3c--playful-pavlova-3da57a.netlify.app)
+
+<img width="1417" alt="home" src="https://github.com/RevoU-FSSE-2/week-7-gkorompis/assets/52250424/b222270e-9a3a-4323-a03d-bb188b40a405">
+
+## Introduction
+
+Wisespent is a user-friendly Finance App designed to help you track your cash flow effectively. It offers a simple and intuitive interface to record your income and expenses, providing you with valuable insights into your financial habits.
+
+### Main Feature - Record
+
+The primary feature of Wisespent is the "Record" section, where you can easily add and manage your income and expenses. You can categorize your transactions, add notes, and track your financial activities over time.
+### Section Cash Flow directs to tracking page
+<img width="1419" alt="cashflow" src="https://github.com/RevoU-FSSE-2/week-7-gkorompis/assets/52250424/0eb8e053-fcd1-42a7-a9a8-457bb6110733">
+
+### Cash Flow page to track spending
+<img width="1418" alt="track" src="https://github.com/RevoU-FSSE-2/week-7-gkorompis/assets/52250424/bfe21a0e-17fa-401e-bd26-28453566a3b2">
+
+### Form to add new record
+<img width="1416" alt="form" src="https://github.com/RevoU-FSSE-2/week-7-gkorompis/assets/52250424/a74d37e4-0792-4c86-96e3-e7c62fa1871c">
+
+### List Records
+<img width="1402" alt="listrecords" src="https://github.com/RevoU-FSSE-2/week-7-gkorompis/assets/52250424/646e1e2a-f126-40d4-a191-55ed3a3ac3d0">
+
+## Dependencies
+
+Wisespent is built using web technologies - HTML, vanilla CSS, and TypeScript. The following dependencies are required to run the app:
+
+- HTML: The standard markup language for creating the structure of the app's user interface.
+- CSS: Cascading Style Sheets for styling the app and ensuring a visually appealing user experience.
+- TypeScript: A superset of JavaScript, used for DOM manipulation and managing the application's logic.
+
+## Workflow
+
+To set up the project locally and compile the TypeScript files, follow these steps:
+
+1. Clone the repository from GitHub:
+```
+git clone https://github.com/RevoU-FSSE-2/week-7-gkorompis
+```
+2. Navigate to the project folder:
+```
+cd week-7-gkorompis/src
+```
+3. Install the necessary dependencies:
+```
+npm install
+```
+4. Run the TypeScript compiler to transpile the source files to JavaScript:
+```
+npx tsc
+```
+
+The compiled JavaScript files will be outputted to the `dist` folder, as configured in the `tsconfig.json`.
+
+## Deployment using Netlify
+
+Wisespent can be easily deployed using Netlify, a popular platform for hosting static websites. Follow these steps to deploy the app:
+
+1. Connect to Git provider
+2. Pick a repository
+3. Site configuration and deploy
+
+## Contact
+
+For any questions or support, please contact us at support@wisespentapp.com.
+
+Happy budgeting with Wisespent! 🌟
---
+++ client/src/transactions/transactions.ts
@@ -0,0 +1,124 @@
+console.log("ts: transactions");
+// Add New Transactions ---------------------------------------------------------------------------------
+// button new transaction handler
+const newTransactionButtonHandler = (event: MouseEvent) => {
+    let boardModalContainer = document.getElementById('board-modal-container');
+    boardModalContainer?.classList.toggle("board-modal-container-hide")
+    let boardModal = document.getElementById('section-service-board-modal');
+    boardModal?.classList.toggle("board-modal-blur-background");
+};
+
+// add event listener to button new transaction
+const newTransactionButton = document.getElementById("btn-new-transaction");
+newTransactionButton?.addEventListener('click', newTransactionButtonHandler);
+
+
+
+//Submit New Transaction ---------------------------------------------------------------------------------
+interface NewTransactionPayload {
+   transactionAmount?: Number;
+   transactionDate?: String;
+   transactionWallet?: String;
+   transactionPocket?: String;
+   transactionTag?: String;
+   transactionDetails?: String;
+}
+const postNewTransaction=()=>{
+    // let contentEmpty = document.getElementById('content-empty');
+    // if(contentEmpty){
+    //     document.getElementById('board-content-new-record')?.replaceChildren()
+    //     console.log('element inner is cleaned')
+    // }
+    let payload = <NewTransactionPayload> {};
+    let formRequestAmount = document.getElementById('form-request-amount') as HTMLInputElement | null;
+    payload.transactionAmount = Number(formRequestAmount?.value); 
+    let formRequestDate = document.getElementById('form-request-date') as HTMLSelectElement | null;
+    payload.transactionDate = formRequestDate?.value;
+    let formRequestWallet = document.getElementById('form-request-wallet') as HTMLSelectElement | null;
+    payload.transactionWallet = formRequestWallet?.options[formRequestWallet.selectedIndex].value;
+    let formRequestPocket = document.getElementById('form-request-pocket') as HTMLSelectElement | null;
+    payload.transactionPocket = formRequestPocket?.options[formRequestPocket.selectedIndex].value;
+    let formRequestTag= document.getElementById('form-request-tag') as HTMLInputElement | null;
+    payload.transactionTag= formRequestTag?.value;
+    let formRequestDetails= document.getElementById('form-request-details') as HTMLInputElement | null;
+    payload.transactionDetails= formRequestDetails?.value;
+    // alert(`New Transaction ${payload.transactionDate}: ${payload.transactionAmount} has been submitted`);
+    console.log(">>>payload", payload)
+    return payload;
+};
+const addTransactionCards = () =>{
+    //get payloads
+    const payloads = [{
+        transactionAmount: 100000,
+        transactionDate: '01-01-1996',
+        transactionWallet: "Monthly Income",
+        transactionPocket: "Entertainment",
+        transactionTag: "Netflix",
+        transactionDetails: "",
+    },
+    {
+        transactionAmount: 524000,
+        transactionDate: '01-01-1996',
+        transactionWallet: "Monthly Income",
+        transactionPocket: "Entertainment",
+        transactionTag: "Netflix",
+        transactionDetails: "",
+    }]
+    let boardCardTransactions = document.getElementById('board-card-transactions');
+    // console.log(">>>board card transactions", boardCardTransactions);
+    boardCardTransactions?.replaceChildren();
+    
+    payloads.map(payload =>{
+        // append element
+        let cardContainer = document.createElement('div');
+        cardContainer.setAttribute('class','card-container');
+        let cardSpan = document.createElement('div');
+        cardSpan.setAttribute('class','card-span');
+        let newCard = document.createElement('div');
+        newCard.setAttribute('class','record-card');
+
+        let cardTitle = document.createElement('h4');
+        let cardCount = document.querySelector('.board-content-container')?.children.length
+        let transactionAmount = document.createElement('p');
+        transactionAmount.innerText = `Transaction Owner: ${payload.transactionAmount}`;
+        let transactionDate = document.createElement('p');
+        transactionDate.innerText = `Transaction Type: ${payload.transactionDate}`;
+        let transactionWallet = document.createElement('p');
+        transactionWallet.innerText = `Amount: ${payload.transactionWallet}`;
+        let transactionPocket= document.createElement('p');
+        transactionPocket.innerText = `Pocket: ${payload.transactionPocket}`;
+        let transactionTag= document.createElement('p');
+        transactionTag.innerText = `Tag: ${payload.transactionTag}`;
+        let transactionDetails = document.createElement('p');
+        transactionDetails.innerText = `Details: ${payload.transactionDetails}`;
+
+        cardTitle.textContent = `Record #00-${Number(cardCount) + 1}`;
+        newCard.appendChild(cardTitle);
+        newCard.appendChild(transactionAmount);
+        newCard.appendChild(transactionDate);
+        newCard.appendChild(transactionWallet);
+        newCard.appendChild(transactionPocket);
+        newCard.appendChild(transactionTag);
+        console.log('>>new card', newCard);
+
+        let boardCardTransactions = document.getElementById('board-card-transactions');
+        cardContainer?.appendChild(cardSpan);
+        cardContainer?.appendChild(newCard);
+        boardCardTransactions?.appendChild(cardContainer);
+        console.log('>>list transactions', boardCardTransactions);
+
+        }) 
+};
+const submitTransactionButtonHandler = (event: MouseEvent) => {
+    console.log('submit')
+    let boardModalContainer = document.getElementById('board-modal-container');
+    boardModalContainer?.classList.toggle("board-modal-container-hide")
+    let boardModal = document.getElementById('section-service-board-modal');
+    boardModal?.classList.toggle("board-modal-blur-background");
+    postNewTransaction();
+    addTransactionCards();
+};
+
+// add event listener to button submit new transaction
+const submitAssetButton = document.getElementById("btn-submit-asset");
+submitAssetButton?.addEventListener('click', submitTransactionButtonHandler);
